1f2d3d1701282376b7eac575ae3eb147971027b8
Artyom Kosmin <artyom17may@gmail.com>
2021-07-29 21:42:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+try {
+    var form = document.querySelector('#form');
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        console.log('submit');
+    });
+
+} catch(e) {
+    console.error('form error');
+}
+
