598cf52853712ae06a1ab5481b3bbdde38236d2f
Artyom Kosmin <artyom17may@gmail.com>
2021-08-03 19:09:10 +0300

    Второй урок

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-let money = 100;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 600;
+let income = '100';
+let addExpenses = '20, 10, 15';
+let deposit = true;
+let mission = 1500;
+let period = 5;
 
-alert('Hi');
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
 
-console.log('Text');
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' долларов');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
 
 
 
