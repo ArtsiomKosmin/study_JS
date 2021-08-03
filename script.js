0a1c13f5f76996e4b8b2148c03858d1234175822
Artyom Kosmin <artyom17may@gmail.com>
2021-08-03 17:47:03 +0300

    Поместил индекс и скрипт в ветку dev

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-'use strict';
-try {
-    var form = document.querySelector('#form');
-
-    form.addEventListener('submit', e => {
-        e.preventDefault();
-        console.log('submit');
-    });
-
-} catch(e) {
-    console.error('form error');
-}
+alert ("hello");
 
