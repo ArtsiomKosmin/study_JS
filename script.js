b3b3187b034f8c332439b5be43d2150e61a35234
Artsiom Kosmin <artyom17may@gmail.com>
2021-08-04 22:58:23 +0300

    Урок 3

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let money = 600;
+let money = +prompt('Ваш месячный доход?');
 let income = '100';
-let addExpenses = '20, 10, 15';
-let deposit = true;
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+let deposit = confirm('Есть ли у вас депозит в банке?');
 let mission = 1500;
 let period = 5;
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let amount1 = +prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount2 = +prompt('Во сколько это обойдется?');
+let budgetMonth = amount1 + amount2;
+
+
 
 console.log(typeof(money));
 console.log(typeof(income));
@@ -16,7 +23,22 @@ console.log('Цель заработать ' + mission + ' долларов');
 
 console.log(addExpenses.toLowerCase().split(', '));
 
-let budgetDay = money / 30;
+let budgetDay = budgetMonth / 30;
+console.log(Math.floor(budgetDay));
+
+
+console.log(budgetMonth);
+console.log(Math.ceil(budgetMonth / mission));
+
+if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay < 1200 && budgetDay > 600 ) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay < 600 && budgetDay > 0) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else {
+    console.log('Что то пошло не так');
+}
 console.log(budgetDay);
 
 
